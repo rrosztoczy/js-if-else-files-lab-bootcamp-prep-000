277fdefb7e3cc1b55a1bd874b09b3cb55e6ac677
Ryan Rosztoczy <rrosztoczy@gmail.com>
2018-11-15 23:57:19 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var height = "74"
 if (name === "Steve") {
   console.log("The name is Correct")
 } else {
-  console.log("Expected Name to be Susan")
+  console.log("Expected Name to be Steve")
 }
 
 // Don't worry about this
